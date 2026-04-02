4cfe3d7e0ccf6fef2af8f065e7cebd0dcebe3770
Sameer <sharief.k.sameer007@gmail.com>
2026-04-02 19:12:31 +0530

    Updated Few files

--- src/test/java/com/api/tests/MasterAPITest.java
+++ src/test/java/com/api/tests/MasterAPITest.java
@@ -1,16 +1,16 @@
-package com.api.tests;
-
-import org.testng.annotations.Test;
-
-import com.api.utils.ConfigManager2;
-
-import static io.restassured.RestAssured.*;
-
-public class MasterAPITest {
-	
-	@Test
-	public void masterAPITest() {
-		given().baseUri(ConfigManager2.getProperty(DEFAULT_BODY_ROOT_PATH));
-	}
-
-}
+//package com.api.tests;
+//
+//import org.testng.annotations.Test;
+//
+//import com.api.utils.ConfigManager2;
+//
+//import static io.restassured.RestAssured.*;
+//
+//public class MasterAPITest {
+//	
+//	@Test
+//	public void masterAPITest() {
+//		given().baseUri(ConfigManager2.getProperty(DEFAULT_BODY_ROOT_PATH));
+//	}
+//
+//}
