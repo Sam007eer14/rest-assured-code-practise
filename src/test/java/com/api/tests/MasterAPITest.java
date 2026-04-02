4952fb335a8894f9d7acbf1ae697e68cd24e6301
Sameer <sharief.k.sameer007@gmail.com>
2026-04-02 18:02:08 +0530

    Git First code Push

--- src/test/java/com/api/tests/MasterAPITest.java
+++ src/test/java/com/api/tests/MasterAPITest.java
@@ -2,11 +2,15 @@ package com.api.tests;
 
 import org.testng.annotations.Test;
 
+import com.api.utils.ConfigManager2;
+
+import static io.restassured.RestAssured.*;
+
 public class MasterAPITest {
 	
 	@Test
 	public void masterAPITest() {
-		
+		given().baseUri(ConfigManager2.getProperty(DEFAULT_BODY_ROOT_PATH));
 	}
 
 }
