9dfafbd235ea2762d5b65b8d8b87e752f1c6bf28
Sameer <sharief.k.sameer007@gmail.com>
2026-04-26 12:59:47 +0530

    UpdatedProject

---
+++ src/test/java/com/api/utils/UserBean.java
@@ -0,0 +1,45 @@
+package com.api.utils;
+
+
+import com.opencsv.bean.CsvBindByName;
+import com.poiji.annotation.ExcelCellName;
+
+public class UserBean {
+    @ExcelCellName("username")
+    @CsvBindByName(column = "username")
+    private String username;
+
+    @ExcelCellName("password")
+    @CsvBindByName(column = "password")
+    private String password;
+
+    public UserBean() {
+    }
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserBean(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserBean [username=" + username + ", password=" + password + "]";
+	}
+}
